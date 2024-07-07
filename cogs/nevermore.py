bccd69378de39ba3ab2be564e93e30dba3488a65
CrazyPranav <90861842+CrazyPranav@users.noreply.github.com>
2024-07-07 16:50:43 +0530

    Update nevermore.py

--- cogs/nevermore.py
+++ cogs/nevermore.py
@@ -17,7 +17,7 @@ class nevermore(commands.Cog):
     if guild.id == 734418473580494869:
       embed = discord.Embed(title=" <a:ace_heart:865258867234570250> Welcome to Skyline 𖤓  <a:ace_heart:865258867234570250>",color = 0x2f3136, description=f" <a:REAL_RightArrow:865840196927225886> Account Created : <t:{int(member.created_at.timestamp())}:f> \n<a:REAL_RightArrow:865840196927225886> [Rules](https://discord.gg/9jAhQkkUgB) • [Self Roles](https://discord.gg/Etj68bKJJz) • [Drops](https://discord.gg/6DwHyVXkVj)")
       embed.set_footer(text = f" You are {len(guild.members)}th member! | {member}",icon_url=member.display_avatar.url)
-      embed.set_image(url= "https://cdn.discordapp.com/attachments/913514091677614160/1259421272652382298/IMG_20240707_134017.jpg?ex=668b9ef6&is=668a4d76&hm=af5843ad94330d4fe59e29339450fd500fd113325becfa3a52fca1a6a0c20d01&")
+      embed.set_image(url= "https://cdn.discordapp.com/attachments/913514091677614160/1259414329799741521/VN20240707_121916-ezgif.com-video-to-gif-converter.gif?ex=668b987f&is=668a46ff&hm=9d9a1e6d472d9ee28fdbb28e0f1d60b9faeefc759da4846ed190849a41071e55&")
       channel = self.bot.get_channel( self.logging_channel_id )
       rules = self.bot.get_channel(self.rules_channel)
       srole = self.bot.get_channel(self.self_roles)
